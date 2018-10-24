49cf0ea0d41b51725c1a8c255a8c955897627a64
Markus Pikkanen <markus.pikkanen@gmail.com>
2018-10-24 19:08:13 +0300

    lab1 vamis mutta tulokset kummallisia

--- src/lab01/Main.java
+++ src/lab01/Main.java
@@ -1,35 +1,75 @@
 package lab01;
 
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
-    private static final int N = 6;
-    private static final int STARTN = 1000;
-    private static final int INCN = 500;
-    private static final String OUTPUTFILE = "./output/data.csv";
+    private static final int N = 10;
+    private static final int STARTN = 10000;
+    private static final int INCN = 5000;
+    private static final String OUTPUTFILE = "./output/data-str.csv";
 
     public static void main(String[] args) {
 
-        Stopwatch sw = new Stopwatch();
 
-        Integer[] intList = generateRandomArray(STARTN);
-        Arrays.sort(intList);
+        try {
+            int arrayN = STARTN;
+            PrintWriter writer = new PrintWriter(OUTPUTFILE, "UTF-8");
+            Stopwatch sw = new Stopwatch();
+            writer.println("N;Linear;Binary");
 
-        MySearch lin = new MySearch(false); lin.setArray(intList);
-        MySearch bin = new MySearch(true);  bin.setArray(intList);
+            for (int i = 0; i < N; i++) {
+                System.out.println("round: " + i);
+                String[] intList = generateRandomStringArray(arrayN);
+                Arrays.sort(intList);
 
-        sw.measure(lin);
+                StringBuilder result = new StringBuilder();
+                result.append(arrayN);
+
+                MySearch.Linear lin = new MySearch.Linear();
+                lin.setArray(intList); lin.setSearchables();
+
+                MySearch.Binary bin = new MySearch.Binary();
+                bin.setArray(intList); bin.setSearchables();
+
+                sw.measure(lin); result.append(";"); sw.toValue(result);
+                System.out.println("linear to value: " + sw.toValue());
+
+                sw.measure(bin); result.append(";"); sw.toValue(result);
+                System.out.println("binary to value: " + sw.toValue());
+
+                writer.println(result.toString());
+                arrayN += INCN;
+            }
+            writer.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static Integer[] generateRandomArray(int n){
+    public static Integer[] generateRandomIntegerArray(int n){
         Integer[] list = new Integer[n];
         Random random = new Random();
 
         for (int i = 0; i < n; i++)
         {
             list[i] = random.nextInt();
+        }
+        return list;
+    }
+
+    public static String[] generateRandomStringArray(int n){
+        String[] list = new String[n];
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++)
+        {
+            byte[] array = new byte[2];
+            random.nextBytes(array);
+            list[i] = new String(array, Charset.forName("UTF-8"));
         }
         return list;
     }
--- src/lab01/MySearch.java
+++ src/lab01/MySearch.java
@@ -1,63 +1,81 @@
 package lab01;
 
-public class MySearch implements Stopwatch.Test {
+import java.util.Random;
 
-    private Comparable[] array;
-    private boolean binaryS;
-    private Integer find;
+public class MySearch {
+    public static class Binary implements Stopwatch.Test {
 
-    public MySearch(boolean binarySearch) {
-        this.binaryS = binarySearch;
-        find = 0;
-    }
+        private Comparable[] array;
+        private Comparable[] searchFor;
 
-    public void setFind(Integer integer) {
-        find = integer;
-    }
+        int search(Comparable elem) {
+            int low = 0;
+            int high = array.length - 1;
 
-    void setArray(Comparable[] array) {
-        this.array = array;
-    }
-
-    int LinearSearch(Comparable elem) {
-        for (int i = 0; i < array.length; i++) {
-            if (elem.compareTo(array[i]) == 0) {
-                return i;
+            while (low <= high) {
+                int ix = (low + high) / 2;
+                if (elem.compareTo(array[ix]) == 0) {
+                    return ix;
+                } else if (elem.compareTo(array[ix]) > 0) {
+                    low = ix + 1;
+                } else {
+                    high = ix - 1;
+                }
             }
+            return -1;
         }
-        return -1;
-    }
 
-    int BinarySearch(Comparable elem) {
-        int low = 0;
-        int high = array.length - 1;
-
-        while (low <= high) {
-            int ix = (low + high) / 2;
-            if (elem.compareTo(array[ix]) == 0) {
-                return ix;
-            } else if (elem.compareTo(array[ix]) > 0) {
-                low = ix + 1;
-            } else {
-                high = ix - 1;
-            }
+        public void setArray(Comparable[] array) {
+            this.array = array;
         }
-        return -1;
-    }
 
-    @Override
-    public void test() {
-        if (binaryS) {
+        public void setSearchables() {
+            Random r = new Random();
+            searchFor = new Comparable[500];
             for (int i = 0; i < 500; i++) {
-                BinarySearch(find);
+                searchFor[i] = array[r.nextInt(array.length - 1)];
             }
-        } else {
-            for (int i = 0; i < 500; i++) {
-                LinearSearch(find);
+        }
+
+        @Override
+        public void test() {
+            for (Comparable e: searchFor) {
+                search(e);
             }
         }
     }
 
+    public static class Linear implements Stopwatch.Test {
 
+        private Comparable[] array;
+        private Comparable[] searchFor;
 
+        int search(Comparable elem) {
+            for (int i = 0; i < array.length; i++) {
+                if (elem == array[i]) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void setArray(Comparable[] array) {
+            this.array = array;
+        }
+
+        public void setSearchables() {
+            Random r = new Random();
+            searchFor = new Comparable[500];
+            for (int i = 0; i < 500; i++) {
+                searchFor[i] = array[r.nextInt(array.length - 1)];
+            }
+        }
+
+        @Override
+        public void test() {
+            for (Comparable e: searchFor) {
+                search(e);
+            }
+        }
+    }
 }
